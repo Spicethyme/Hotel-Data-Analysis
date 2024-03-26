9702474688aa5ba8dfb4efdae32ae13dd291ff43
Sebastien <88175757+Spicethyme@users.noreply.github.com>
2024-03-26 19:07:18 -0400

    Add files via upload

--- sql_alchemy.py
+++ sql_alchemy.py
@@ -1,9 +1,9 @@
 import pandas as pd 
 from sqlalchemy import create_engine
 
-engine = create_engine('postgresql://postgres:Tisagoodmoon@localhost:5432/postgres')
+engine = create_engine('postgresql://postgres:password@localhost:5432/postgres')
 
-"""
+
 df = pd.read_csv('2018.csv')
 df.to_sql('2018', engine, if_exists='replace')
 
@@ -12,7 +12,6 @@ df.to_sql('2019', engine, if_exists='replace')
 
 df = pd.read_csv('2020.csv')
 df.to_sql('2020', engine, if_exists='replace')
-"""
 
 df = pd.read_csv('meal_cost.csv')
 df.to_sql('meal_cost', engine, if_exists='replace')
